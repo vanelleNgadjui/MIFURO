fd77899ac37b9d26b7a993b936bc4c033cb9d17e
vanelleNgadjui <vanellejob@gmail.com>
2023-06-10 16:34:11 +0200

    modif v3

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,28 +1,10 @@
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+
+import React from 'react';
 import Layout from '../components/layout';
-import '../styles/about.css';
 
 const About: React.FC = () => {
-  const [text, setText] = useState('');
 
-  useEffect(() => {
-    const fullText = "Ce site est en construction...";
-    let currentIndex = 0;
-
-    const intervalId = setInterval(() => {
-      if (currentIndex < fullText.length) {
-        setText(fullText.substring(0, currentIndex + 1));
-        currentIndex++;
-      } else {
-        clearInterval(intervalId);
-      }
-    }, 100); // Vitesse de frappe du texte (en millisecondes)
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+ 
     return (
      <Layout>
     </Layout>
